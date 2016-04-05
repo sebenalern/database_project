ab1e619f3145d2c6cb374cc26c2bca67a617644c
LailaShaikh <shaikhlaila26@gmail.com>
2016-04-05 01:06:46 +0000

    some controller and server functions added:

--- server.py
+++ server.py
@@ -12,12 +12,62 @@ from psycopg2.extensions import adapt
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 
+
+
+def connectToDB():
+    connectionString = 'dbname = chitchat user=chitchat_role password=password host=localhost'
+    try:
+        return psycopg2.connect(connectionString)
+    except:
+        print 'Can\'t connect to the database.'
+
+
 #create socketio app
 socketio = SocketIO(app)
+
+
+@app.route('/profile', methods=['GET', 'POST'])
+def renderProfile(): 
+    if request.method == 'POST':
+        conn=connectToDB()
+        cur=conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        try:
+            print 'hello'
+            myUuid=uuid.uuid1()
+            print(cur.mogrify("insert into users values(%s,%s,%s,%s,%s,crypt(%s, gen_salt('bf')));",(str(myUuid), request.form['email'],request.form['firstname'], request.form['lastname'],request.form['username'],request.form['password'])))
+            cur.execute("insert into users values(%s,%s,%s,%s,%s,crypt(%s,gen_salt('bf')));",(str(myUuid),request.form['email'],request.form['firstname'], request.form['lastname'],request.form['username'],request.form['password']))
+            conn.commit()
+        except:
+            conn.rollback()
+    #socketio.emit('receiveUserProfileData', {"username": "Nick"})
+    
+
+
+
+@socketio.on("LoginDetails", namespace="/chitchat")
+def renderLoginPage(UserDetails):
+    print 'helooooooo we are in login page------------'
+    try:
+        conn=connectToDB()
+        cur=conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        print(cur.mogrify("select email, first_name,last_name,username from users where email=%s AND password=crypt(%s, password);",(UserDetails[0],UserDetails[1])))
+        cur.execute("select email, first_name, last_name, username from users where email=%s AND password=crypt(%s, password);",(UserDetails[0],UserDetails[1]))
+        print 'we have reached here'
+        loginQueryFetch=cur.fetchone()
+        print loginQueryFetch
+        if loginQueryFetch is not None:
+            emit("receiveUserProfileData", loginQueryFetch, namespace = '/chitchat')
+            
+        else:
+            emit("notReceiveUserProfileData", namespace = '/chitchat')
+    except:
+        print 'could not excess login table'
+
 
 @app.route("/")
 def index():
     return send_file("templates/index.html")
+    
     
 if __name__ == '__main__':
    socketio.run(app, host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)), debug=True)
--- static/js/controller.js
+++ static/js/controller.js
@@ -13,23 +13,23 @@ chitChatApp.config(['$routeProvider',
         
              when('/', {
                  templateUrl: '../static/partials/mainPageLayout.html/',
-                 controller: "mainController"
+                 controller: "chitChatApp"
              }).
             when('/login', {
                  templateUrl: '../static/partials/loginPage.html/',
-                 controller: "mainController"
+                 controller: "chitChatApp"
              }).
             when('/registration', {
                  templateUrl: '../static/partials/registration.html/',
-                 controller: "mainController"
+                 controller: "chitChatApp"
              }).
             when('/profile', {
                  templateUrl: '../static/partials/profile.html/',
-                 controller: "mainController"
+                 controller: "chitChatApp"
              }).     
              when('/edit_profile', {
                  templateUrl: '../static/partials/edit_profile.html/',
-                 controller: "mainController"
+                 controller: "chitChatApp"
              }).             
              otherwise({
                  redirectTo: '/'
@@ -37,5 +37,42 @@ chitChatApp.config(['$routeProvider',
     }]);
     
 chitChatApp.controller('chitChatApp', ['$scope', '$log', function ($scope, $log) {
+
+
+    //---sending the login details to be checked from the database in the server side
+     $scope.getLogin = function (email, password) {
+      $log.log(email);
+      $log.log(password);
+      $scope.tempArray = [];
+      $scope.tempArray.push(email);
+      $scope.tempArray.push(password);
+      $log.log($scope.hello);
+      socket.emit('LoginDetails', $scope.tempArray);
+    };
+    
+    //---getting the registration detail from html page
+    $scope.RegistrationCheck = function (UserNameR,FirstNameR, LastNameR, EmailR, PasswordR) {
+        $log.log("Hey nickkkkkkkkkkkkk");
+
+        socket.emit('InsertRegistrationDetails',UserNameR,FirstNameR, LastNameR, EmailR, PasswordR);
+    };
+    
+    
+    //matched user details ---------------------------------
+     socket.on('receiveUserProfileData', function(userData) {
+        $log.log("in receiveUserProfileData");
+        
+    });
+    
+    
+    //unmatched user details---------------------------
+    socket.on('notReceiveUserProfileData', function() {
+        $log.log("inside notReceiveUserProfileData");
+        $scope.notLoggedIn = true;
+    });
+    
+    
+    
+    
     
 }]);
